c1bdbd4dba138ee35760d75e9b0ccf3bff9e9819
Kohsuke Kawaguchi <kk@kohsuke.org>
2014-02-13 08:44:04 -0800

    added a callback mechanism

--- src/main/java/org/jenkinsci/plugins/deployment/DeploymentFacet.java
+++ src/main/java/org/jenkinsci/plugins/deployment/DeploymentFacet.java
@@ -19,5 +19,8 @@ public class DeploymentFacet extends FingerprintFacet {
     public void add(HostRecord r) throws IOException {
         records.add(r);
         getFingerprint().save();
+        for (DeploymentFacetListener l : DeploymentFacetListener.all()) {
+            l.onChange(this,r);
+        }
     }
 }
---
+++ src/main/java/org/jenkinsci/plugins/deployment/DeploymentFacetListener.java
@@ -0,0 +1,21 @@
+package org.jenkinsci.plugins.deployment;
+
+import hudson.ExtensionList;
+import hudson.ExtensionPoint;
+import jenkins.model.Jenkins;
+
+/**
+ * Listens to the addition of new {@link HostRecord}.
+ *
+ * @author Kohsuke Kawaguchi
+ */
+public abstract class DeploymentFacetListener implements ExtensionPoint {
+    /**
+     * Called when {@link DeploymentFacet} gets a new {@link HostRecord}.
+     */
+    public abstract void onChange(DeploymentFacet facet, HostRecord newRecord);
+
+    public static ExtensionList<DeploymentFacetListener> all() {
+        return Jenkins.getInstance().getExtensionList(DeploymentFacetListener.class);
+    }
+}
