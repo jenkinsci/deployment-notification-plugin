a33f9d8fac18ae2ecb96067940ec644b5c3a02d5
Kohsuke Kawaguchi <kk@kohsuke.org>
2014-02-14 06:25:15 -0800

    doc improvement

--- src/main/java/org/jenkinsci/plugins/deployment/UpstreamDeploymentCause.java
+++ src/main/java/org/jenkinsci/plugins/deployment/UpstreamDeploymentCause.java
@@ -4,6 +4,11 @@ import hudson.model.Cause.UpstreamCause;
 import hudson.model.Run;
 
 /**
+ * Indicates that a build was triggered because an artifact from a previous build was deployed to servers.
+ *
+ * <p>
+ * This class extends from {@link UpstreamCause} to point to the build that previously touched the artifact in question.
+ *
  * @author Kohsuke Kawaguchi
  */
 public class UpstreamDeploymentCause extends UpstreamCause {
