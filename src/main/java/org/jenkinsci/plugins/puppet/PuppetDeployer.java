a46a101a5e74dbc1646b445d797a475e358570e1
Kohsuke Kawaguchi <kk@kohsuke.org>
2014-02-15 10:48:39 -0800

    Depeting puppet specific parts

--- src/main/java/org/jenkinsci/plugins/puppet/PuppetDeployer.java
+++
@@ -1,8 +0,0 @@
-package org.jenkinsci.plugins.puppet;
-
-/**
- * @author Kohsuke Kawaguchi
- */
-public class PuppetDeployer {
-    // TODO: like chef-plugin, it's nice to be able to have a publisher that deploys
-}
