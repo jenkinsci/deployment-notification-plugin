0c8f83d112c731f5bfe46d49e316ea3734878f3b
Kohsuke Kawaguchi <kk@kohsuke.org>
2014-02-16 06:28:19 -0800

    added equality check

--- src/main/java/org/jenkinsci/plugins/deployment/HostRecord.java
+++ src/main/java/org/jenkinsci/plugins/deployment/HostRecord.java
@@ -23,12 +23,16 @@ public class HostRecord {
     private final String replaces;
 
 
-    public HostRecord(String host, String env, String path, String replaces) {
-        this.timestamp = System.currentTimeMillis();
+    public HostRecord(long timestamp, String host, String env, String path, String replaces) {
+        this.timestamp = timestamp;
         this.host = host;
         this.env = env;
         this.path = path;
         this.replaces = replaces;
+    }
+
+    public HostRecord(String host, String env, String path, String replaces) {
+        this(System.currentTimeMillis(),host,env,path,replaces);
     }
 
     public String getHost() {
@@ -64,5 +68,31 @@ public class HostRecord {
     public String getTimestampString() {
         long duration = System.currentTimeMillis()-timestamp;
         return Util.getPastTimeString(duration);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        HostRecord that = (HostRecord) o;
+
+        if (timestamp != that.timestamp) return false;
+        if (env != null ? !env.equals(that.env) : that.env != null) return false;
+        if (host != null ? !host.equals(that.host) : that.host != null) return false;
+        if (path != null ? !path.equals(that.path) : that.path != null) return false;
+        if (replaces != null ? !replaces.equals(that.replaces) : that.replaces != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = host != null ? host.hashCode() : 0;
+        result = 31 * result + (env != null ? env.hashCode() : 0);
+        result = 31 * result + (int) (timestamp ^ (timestamp >>> 32));
+        result = 31 * result + (path != null ? path.hashCode() : 0);
+        result = 31 * result + (replaces != null ? replaces.hashCode() : 0);
+        return result;
     }
 }
